48199b5a29a05c3b1480df482dc368facbb222f6
Célian | VullWen <celian.pinquier@gmail.com>
2025-02-16 15:01:09 +0100

    C'est la créature

--- main.py
+++ main.py
@@ -6,34 +6,46 @@ from scapy.all import *
 def create_access_point(interface):
     print(f"Création d'un point d'acces wifi sur l'interface {interface}")
     ssid = "FreeWifi :D"
-    password = "test"
+    password = "SajedCalvitie1"
+    channel = 6
     
-    # Génération du fichier de configuration hostapd pour WPA (non WPA2)
+    # Configuration de l'interface
     hostapd_config = f"""
 interface={interface}
-driver=nl80211
 ssid={ssid}
 hw_mode=g
-channel=6
-wpa=1
+channel={channel}
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
 wpa_passphrase={password}
 wpa_key_mgmt=WPA-PSK
-wpa_pairwise=TKIP
-    """.strip()
+rsn_pairwise=CCMP
+    """
     
-    config_file = "hostapd.conf"
-    try:
-        with open(config_file, "w") as f:
-            f.write(hostapd_config)
-        print(f"Configuration hostapd écrite dans {config_file}")
+    with open("/tmp/hostapd.conf", "w") as config_file:
+        config_file.write(hostapd_config)
         
-        # Démarrage de hostapd avec le fichier de configuration généré
-        subprocess.run(["hostapd", config_file], check=True)
+    dnsmask_config = f"""
+interface={interface}
+dhcp-range=192.168.150.10,192.168.150.50,255.255.255.0,12h
+    """
+    
+    with open("/tmp/dnsmasq.conf", "w") as config_file:
+        config_file.write(dnsmask_config)
+        
+    # Démarrage des services
+    try:
+        subprocess.run(["sudo", "hostapd", "/tmp/hostapd.conf"], check=True)
+        print(f"Point d'acces wifi {ssid} démarré avec succès sur l'interface {interface}")
+        
+        subprocess.run(["sudo", "dnsmasq", "-C", "/tmp/dnsmasq.conf", "-d"], check=True)
+        print("Serveur DHCP démarré avec succès") 
         
     except subprocess.CalledProcessError as e:
         print(f"Erreur lors du démarrage du point d'acces: {e}")
-    except Exception as ex:
-        print(f"Erreur lors de l'écriture de la configuration: {ex}")
         
     
 
@@ -63,6 +75,7 @@ def main():
     args = parser.parse_args()
     
     if args.create:
+        print(f"Création d'un point d'acces wifi sur l'interface {args.interface}")
         create_access_point(args.interface)
     
     if args.capture:

